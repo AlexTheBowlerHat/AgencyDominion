ce8e1cdeb640f1eeb7cdf456e26cb3cce3cbaa66
AlexTheBowlerHat <66259@selwyn.school.nz>
2022-06-28 09:41:18 +1200

    28/06/22
    -Bullet
    -Health
    -F

--- dominion/Assets/Scripts/HealthScript.cs
+++ dominion/Assets/Scripts/HealthScript.cs
@@ -3,28 +3,23 @@ using System.Collections.Generic;
 using UnityEngine;
 //Derived from Alan Thorn's Health Script
 public class HealthScript : MonoBehaviour
-{   
-    //public enum TagToNum
-   // {
-       // "Player" = 1
-        //"Enemy" = 2
-   // }
-
+{
     public GameObject parented;
     public string parentedTag;
     public float startingHealth = 100f; //Health to begin
     [SerializeField]
     private float _healthPoints = 100f;
-    
+    private string objectTagToString;
+
     public float healthPoints //Property that gets the 
     {
         get { return _healthPoints; }
         set
         {
-            _healthPoints = Mathf.Clamp(value,0f,100f); //Sets value and makes sure its between 0 and 100
-            if(_healthPoints <= 0f)
+            _healthPoints = Mathf.Clamp(value, 0f, 100f); //Sets value and makes sure its between 0 and 100
+            if (_healthPoints <= 0f)
             {
-               // Eliminate(); //Kill Method, differs between player and enemy
+                Eliminate(); //Kill Method, differs between player and enemy
             }
         }
     }
@@ -32,13 +27,14 @@ public class HealthScript : MonoBehaviour
     void Start()
     {
         healthPoints = startingHealth;
+        parented = transform.parent.gameObject;
         parentedTag = parented.tag;
     }
-
-   //[[ void Eliminate()
-    //{
-      // private string objectTagToString = parented.transform.tag.ToString();
-        //switch case with whatever number comes out of a variable 
-    //}
-
+    //private string objectTagToString = parented.transform.gameObject.tag.ToString();
+    void Eliminate()
+    {
+        objectTagToString = parentedTag.ToString();
+        
+    }
+       
 }
