9122160aba701340453444c570563ed1ca3a7171
lexHOME <66259@selwyn.school.nz>
2022-08-19 21:07:30 +1200

    Class added for updating colliders

---
+++ dominion/Assets/Scripts/Collider2DExtensions.cs
@@ -0,0 +1,34 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+//Written by Unity Forums user NinjalSV - http://answers.unity.com/answers/1810252/view.html
+public static class Collider2DExtensions 
+{
+    public static void TryUpdateShapeToAttachedSprite (this PolygonCollider2D collider) 
+    {
+        collider.UpdateShapeToSprite(collider.GetComponent<SpriteRenderer>().sprite);
+    }
+
+    public static void UpdateShapeToSprite (this PolygonCollider2D collider, Sprite sprite) 
+    { 
+        // ensure both valid
+        if (collider != null && sprite != null) {
+            // update count
+            collider.pathCount = sprite.GetPhysicsShapeCount();
+                
+            // new paths variable
+            List<Vector2> path = new List<Vector2>();
+
+            // loop path count
+            for (int i = 0; i < collider.pathCount; i++) {
+                // clear
+                path.Clear();
+                // get shape
+                sprite.GetPhysicsShape(i, path);
+                // set path
+                collider.SetPath(i, path.ToArray());
+                }
+        }
+    }
+}
